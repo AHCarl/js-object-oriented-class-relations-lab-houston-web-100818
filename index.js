3ddb6dbaaebe09a9fb920ec9b3dac50ca4a0bfdf
Alexander Carlin <ahcarlin@gmail.com>
2018-12-06 14:32:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+let driverId = 0
+
+let store = {drivers: [], passengers: [], trips: []}
+
+class Driver {
+  constructor(name) {
+    this.name = name 
+    this.id = ++driverId
+    store.drivers.push(this)
+  }
+  
+  trips() {
+    return store.drivers.id === this.id
+  }
+}
+
+let passengerId = 0
+
+class Passenger {
+  constructor(name) {
+    this.name = name 
+    this.id = ++passengerId
+    store.passengers.push(this)
+  }
+}
+
+let tripId = 0
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id 
+    this.passengerId = passenger.id
+    store.trips.push(this)
+  }
+  
+  driver() {
+    return store.drivers.id == this.driverId
+  }
+}
